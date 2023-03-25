d22c360b28b6e48988240317dfc6a103136efa5e
Jovaughn Rose <jovaughnrose4@gmail.com>
2023-03-25 10:52:09 -0500

    Long class practice in Javascript

---
+++ classes/dragon.js
@@ -0,0 +1,31 @@
+// Your code here
+class Dragon {
+	constructor(name, color) {
+		this.name = name;
+		this.color = color;
+	}
+	breathesFire() {
+		return `${this.name} breathes fire everywhere! BURN!!!!`;
+	}
+
+	static getDragons(...dragons) {
+		dragons = dragons.map((dragon) => dragon.name);
+		return dragons;
+	}
+}
+
+const puff = new Dragon("Puff", "green");
+console.log(puff);
+console.log(puff.breathesFire());
+
+const toothless = new Dragon("Toothless", "black");
+console.log(Dragon.getDragons(puff, toothless));
+
+/****************************************************************************/
+/******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
+
+try {
+	module.exports = Dragon;
+} catch {
+	module.exports = null;
+}
